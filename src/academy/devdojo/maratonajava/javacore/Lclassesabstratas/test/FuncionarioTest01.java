22d012afb6351644cf47db5ae27325bfcbbc9175
Lucca <luccagarciarosa@gmail.com>
2025-04-02 20:52:23 -0300

    Classes Abstratas

---
+++ src/academy/devdojo/maratonajava/javacore/Lclassesabstratas/test/FuncionarioTest01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Lclassesabstratas.test;
+
+import academy.devdojo.maratonajava.javacore.Lclassesabstratas.dominio.Desenvolvedor;
+import academy.devdojo.maratonajava.javacore.Lclassesabstratas.dominio.Funcionario;
+import academy.devdojo.maratonajava.javacore.Lclassesabstratas.dominio.Gerente;
+
+public class FuncionarioTest01 {
+    public static void main(String[] args) {
+        Desenvolvedor desenvolvedor = new Desenvolvedor("Zoro", 2000);
+        Gerente gerente = new Gerente("Nami", 12000);
+        System.out.println(desenvolvedor);
+        System.out.println(gerente);
+
+    }
+}
