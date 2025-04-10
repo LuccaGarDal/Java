7c7d412b22756421e756a0570509ba5c6accf9bb
Lucca <luccagarciarosa@gmail.com>
2025-04-10 15:38:54 -0300

    Atributos e Métodos estáticos

--- src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DatabaseLoader.java
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DatabaseLoader.java
@@ -15,4 +15,8 @@ public class DatabaseLoader implements DataLoader, DataRemover {
     public void checkPermisison() {
         System.out.println("Checando permissões no banco de dados");
     }
+
+    public static void retrieveMaxDataSize() {
+        System.out.println("Dentro do retrieveMaxDataSize da DatabaseLoader");
+    }
 }
