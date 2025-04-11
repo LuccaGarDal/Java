0612fd2da5da72f4261d005e013b47c0653e1447
Lucca <luccagarciarosa@gmail.com>
2025-04-11 20:42:21 -0300

    Polimorfismo 1

---
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/dominio/Tomate.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio;
+
+public class Tomate extends Produto{
+    public static final double IMPOSTO_POR_CENTO_ = 0.06;
+    public Tomate(String nome, double valor) {
+        super(nome, valor);
+    }
+
+    @Override
+    public double calcularImposto() {
+        System.out.println("Calculando o Imposto do Tomate");
+        return this.valor * IMPOSTO_POR_CENTO_;
+    }
+}
