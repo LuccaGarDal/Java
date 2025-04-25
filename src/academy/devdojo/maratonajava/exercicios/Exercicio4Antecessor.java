30a8b9959983fff190073429cec2eae7aeee2632
Lucca <luccagarciarosa@gmail.com>
2025-04-25 19:03:17 -0300

    Exercicio 4

---
+++ src/academy/devdojo/maratonajava/exercicios/Exercicio4Antecessor.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio4Antecessor {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+        int A = entrada.nextInt();
+        int B = A - 1;
+        int C = A + 1;
+        System.out.println("O antecessor de "+A+" é "+B);
+        System.out.println("O sucessor de "+A+" é "+C);
+    }
+}
