b46d4acb34e031e765e1f5a1471af4b9fb02fe41
Lucca <luccagarciarosa@gmail.com>
2025-04-25 18:27:13 -0300

    Exercicio 2

--- src/academy/devdojo/maratonajava/exercicios/Exercicio2Par.java
+++ src/academy/devdojo/maratonajava/exercicios/Exercicio2Par.java
@@ -1,7 +1,27 @@
 package academy.devdojo.maratonajava.exercicios;
 
+import java.util.Scanner;
+
 public class Exercicio2Par {
     public static void main(String[] args) {
-        System.out.println("Master");
+        Scanner entrada = new Scanner (System.in);
+        int A = entrada.nextInt();
+        if (A % 2 == 0) {
+            System.out.println("Este número é par");
+
+        }else {
+            System.out.println("Este número é impar");
+        }
+
+        if (A > 0) {
+            System.out.println("Este número é positivo");
+        }
+        else if (A == 0){
+            System.out.println("Este número é 0, não sendo positivo ou negativo");
+        }
+        else {
+            System.out.println("Este número é negativo");
+        }
+
     }
 }
