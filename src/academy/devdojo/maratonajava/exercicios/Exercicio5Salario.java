b0dbb3105ea2f3579a0d86508f451ed91a8dd874
Lucca <luccagarciarosa@gmail.com>
2025-04-28 15:05:04 -0300

    Exercicio 5

---
+++ src/academy/devdojo/maratonajava/exercicios/Exercicio5Salario.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio5Salario {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double salarioMinimo = 1293.20;
+        System.out.println("Digite o seu salário: ");
+        double salarioUsuario = entrada.nextDouble();
+        double quantidadeDeSalarios = salarioUsuario/salarioMinimo;
+        System.out.println("Você ganha "+quantidadeDeSalarios+" salários mínimos.");
+    }
+}
