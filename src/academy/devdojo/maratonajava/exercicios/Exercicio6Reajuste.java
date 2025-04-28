0616ddf39e1b1d9778dbee5f348d8aea62a89e44
Lucca <luccagarciarosa@gmail.com>
2025-04-28 15:20:56 -0300

    Exercicio 6

---
+++ src/academy/devdojo/maratonajava/exercicios/Exercicio6Reajuste.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio6Reajuste {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite um valor: ");
+        double valor = entrada.nextDouble();
+        double valorReajustado = valor + valor * 0.05;
+        System.out.println("Com um reajuste de 5% o valor fica "+valorReajustado);
+    }
+}
