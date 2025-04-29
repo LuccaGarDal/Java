4a61c9255e8e64c0c4e4e99f9d2ab3f0ddbf4fea
Lucca <luccagarciarosa@gmail.com>
2025-04-29 15:15:28 -0300

    Exercicio 7

---
+++ src/academy/devdojo/maratonajava/exercicios/Exercicio7Booleanos.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio7Booleanos {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        boolean A = entrada.nextBoolean();
+        boolean B = entrada.nextBoolean();
+        if (A) {
+            System.out.println("O primeiro valor é verdadeiro");
+        } else {
+            System.out.println("O primeiro valor é falso");
+        }
+        if (B) {
+            System.out.println("O segundo valor é verdadeiro");
+        } else {
+            System.out.println("O segundo valor é falso");
+        }
+    }
+}
