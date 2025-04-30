d81171873c549ff5396c084c165113b5defe509a
Lucca <luccagarciarosa@gmail.com>
2025-04-30 19:14:31 -0300

    Exercicio 8

---
+++ src/academy/devdojo/maratonajava/exercicios/Exercicio8Decrescente.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.exercicios;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Exercicio8Decrescente {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int A = entrada.nextInt();
+        int B = entrada.nextInt();
+        int C = entrada.nextInt();
+
+        int[] valores = {A, B, C};
+        Arrays.sort(valores);
+
+        System.out.println("Os valores em ordem decrescente ficam: ");
+        for (int i = valores.length - 1; i >= 0; i--) {
+            System.out.println(valores [i] + " ");
+        }
+    }
+}
