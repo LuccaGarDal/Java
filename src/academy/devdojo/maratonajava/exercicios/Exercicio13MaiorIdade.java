52bc5b1fdf15832ee106ff72ea2ca1d260bd840b
Lucca <luccagarciarosa@gmail.com>
2025-05-06 14:50:01 -0300

    Exercicio 13

---
+++ src/academy/devdojo/maratonajava/exercicios/Exercicio13MaiorIdade.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio13MaiorIdade {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o seu nome: ");
+        String nome = entrada.next();
+        System.out.println("Digite a sua idade: ");
+        int idade = entrada.nextInt();
+
+        if (idade >= 18) {
+            System.out.println("O seu nome é "+nome+" e você é maior de idade");
+        } else if (idade > 0){
+            System.out.println("O seu nome é "+nome+" e você é menor de idade");
+        } else {
+            System.out.println("Você digitou uma idade inválida");
+        }
+
+    }
+}
