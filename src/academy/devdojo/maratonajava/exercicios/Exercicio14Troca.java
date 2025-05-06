e6d00f309ae2adb03af85dfecb092badad655220
Lucca <luccagarciarosa@gmail.com>
2025-05-06 15:13:51 -0300

    Exercicio 14

---
+++ src/academy/devdojo/maratonajava/exercicios/Exercicio14Troca.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio14Troca {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite um valor A: ");
+        int A = entrada.nextInt();
+        System.out.println("Digite um valor B: ");
+        int B = entrada.nextInt();
+
+        int temporario = A;
+        A = B;
+        B = temporario;
+        System.out.println("Trocando o valor de A por B, o valor de A é: " + A);
+        System.out.println("Trocando o valor de B por A, o valor de B é: " + B);
+
+    }
+}
