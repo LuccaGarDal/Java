ee43d64f305dc58de6020996f7ef496a3be9f97a
Lucca <luccagarciarosa@gmail.com>
2025-05-09 15:28:48 -0300

    Exercicio 20

---
+++ src/academy/devdojo/maratonajava/exercicios/Exercicio20Tabuada2.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio20Tabuada2 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+        int num = entrada.nextInt();
+        System.out.println("Aqui está a tabuada do número " + num);
+        for (int i = 1; i <= 10; i++) {
+            int result = num * i;
+            System.out.println(num + " X " + i + " = " + result);
+        }
+    }
+}
