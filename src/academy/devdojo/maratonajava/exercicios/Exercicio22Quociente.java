32ae30fe990bc8a13e09a1a932d25ffde067699b
Lucca <luccagarciarosa@gmail.com>
2025-05-12 14:59:30 -0300

    Exercicio 22

---
+++ src/academy/devdojo/maratonajava/exercicios/Exercicio22Quociente.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio22Quociente {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite um valor: ");
+        int A = entrada.nextInt();
+        System.out.println("Digite outro valor");
+        int B = entrada.nextInt();
+        int quociente = A / B;
+        int resto = A % B;
+        System.out.println("O quociente da divisão de "+A+" por "+B+" resulta em "+quociente);
+        System.out.println("O resto da divisão de "+A+" por "+B+" resulta em "+resto);
+    }
+}
