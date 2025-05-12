405683435c67192534158aa1902328a791500f71
Lucca <luccagarciarosa@gmail.com>
2025-05-12 15:15:59 -0300

    Exercicio 23

---
+++ src/academy/devdojo/maratonajava/exercicios/Exercicio23Liquido.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio23Liquido {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Vamos calcular o seu salário líquido");
+        System.out.println("Digite quanto você ganha por hora aula: ");
+        double realHora = entrada.nextDouble();
+        System.out.println("Agora digite quantas aulas no mês você dá: ");
+        double aulasMes = entrada.nextDouble();
+        System.out.println("Por último, digite o percentual de desconto do INSS: ");
+        double percentual = entrada.nextDouble();
+        double salarioBruto = realHora * aulasMes;
+        double porcentagem = percentual / 100;
+        double salarioLiquido = salarioBruto - (salarioBruto * porcentagem);
+        System.out.println("O seu salário líquido é de "+salarioLiquido+ "R$");
+    }
+}
