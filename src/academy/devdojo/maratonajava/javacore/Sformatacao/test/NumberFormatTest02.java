2593e07d1666af2567f2f3f404c506bfa05070b1
Lucca <luccagarciarosa@gmail.com>
2025-07-10 17:47:26 -0300

    NumberFormat (moedas)

--- src/academy/devdojo/maratonajava/javacore/Sformatacao/test/NumberFormatTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Sformatacao/test/NumberFormatTest02.java
@@ -4,25 +4,25 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
 
-public class NumberFormatTest01 {
+public class NumberFormatTest02 {
     public static void main(String[] args) {
         Locale localeBR = new Locale ("pt", "BR");
         Locale localeJP = Locale.JAPAN;
         Locale localeIT = Locale.ITALY;
         NumberFormat [] nfa = new NumberFormat[4];
-        nfa[0] = NumberFormat.getInstance();
-        nfa[1] = NumberFormat.getInstance(localeJP);
-        nfa[2] = NumberFormat.getInstance(localeBR);
-        nfa[3] = NumberFormat.getInstance(localeIT);
-        double valor = 100_000_000.9832;
+        nfa[0] = NumberFormat.getCurrencyInstance();
+        nfa[1] = NumberFormat.getCurrencyInstance(localeJP);
+        nfa[2] = NumberFormat.getCurrencyInstance(localeBR);
+        nfa[3] = NumberFormat.getCurrencyInstance(localeIT);
+        double valor = 1000.2130;
 
         for (NumberFormat numberFormat : nfa) {
             System.out.println(numberFormat.format(valor));
         }
 
-        String valorString = "1000.2130";                         //se houver um caracter inválido depois de algum numero, o codigo so irá imprimir os numeros que aprecerem antes
+        String valorString = "￥1,000";
         try {
-            System.out.println(nfa[0].parse(valorString));
+            System.out.println(nfa[1].parse(valorString));
         } catch (ParseException e) {
             e.printStackTrace();
         }
