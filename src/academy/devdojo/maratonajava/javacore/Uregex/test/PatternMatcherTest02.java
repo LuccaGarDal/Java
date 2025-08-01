e8375109653446317edbff94b34d0f502b0390a0
Lucca <luccagarciarosa@gmail.com>
2025-08-01 19:40:51 -0300

    Pattern e Matcher 2 (corrigido)

--- src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest02.java
@@ -3,10 +3,16 @@ package academy.devdojo.maratonajava.javacore.Uregex.test;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PatternMatcherTest01 {
+public class PatternMatcherTest02 {
     public static void main(String[] args) {
-        String regex = "aba";
-        String texto = "abababa";
+        // \d = Todos os Dígitos
+        // \D = Tudo o que não for dígito
+        // \s = Espaços em branco ou \t \n \f \r
+        // \S = Todos os caracteres excluindo os brancos
+        // \w = a-z/A-Z
+        // \W = Tudo o que não for incluso no \w
+        String regex = "\\d";
+        String texto = "hhjghkj2j3hk2";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(texto);
         System.out.println("texto:  " + texto);
