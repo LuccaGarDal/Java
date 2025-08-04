2f1f290e89beac37b56e729717446225c6c4e00f
Lucca <luccagarciarosa@gmail.com>
2025-08-04 18:58:18 -0300

    Pattern e Matcher 3

---
+++ src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest03.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.javacore.Uregex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest03 {
+    public static void main(String[] args) {
+        // \d = Todos os Dígitos
+        // \D = Tudo o que não for dígito
+        // \s = Espaços em branco ou \t \n \f \r
+        // \S = Todos os caracteres excluindo os brancos
+        // \w = a-z/A-Z
+        // \W = Tudo o que não for incluso no \w
+        // []
+        String regex ="0[xX][0-9a-fA-F]";
+        String texto = "12 0x 0X 0xFFABC 0x109 0x1";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto);
+        System.out.println("texto:  " + texto);
+        System.out.println("índice: 0123456789");
+        System.out.println("regex: " + regex);
+        System.out.println("Posições encontradas");
+        while (matcher.find()) {
+            System.out.print(matcher.start() + " " + matcher.group() + "\n");
+        }
+    }
+}
