b201215af3f364a54c211203252d301314a21e6b
Lucca <luccagarciarosa@gmail.com>
2025-08-10 17:43:07 -0300

    FileReader

--- src/academy/devdojo/maratonajava/javacore/Vio/test/FileReaderTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Vio/test/FileReaderTest01.java
@@ -1,18 +1,21 @@
 package academy.devdojo.maratonajava.javacore.Vio.test;
 
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
-public class FileWriter01 {
+public class FileReaderTest01 {
     public static void main(String[] args) {
         File file = new File("texto.txt");
-        try (FileWriter fw = new FileWriter(file, true)) {
-            fw.write("Testando escrita no txt\nOlá\n");
-            fw.flush();
+        try (FileReader fr = new FileReader(file);) {
+
+            int i;
+            while((i=fr.read()) != -1) {
+                System.out.print((char)i);
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 }
---
+++ src/academy/devdojo/maratonajava/javacore/Vio/test/FileWriterTest01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.Vio.test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterTest01 {
+    public static void main(String[] args) {
+        File file = new File("texto.txt");
+        try (FileWriter fw = new FileWriter(file, true)) {
+            fw.write("Testando escrita no txt\nOlá\n");
+            fw.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
