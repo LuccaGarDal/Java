0796dbefe0a3ed7cb8c97c469df07de194a4a5b3
LuccaGarDal <luccagarciarosa@gmail.com>
2025-09-22 19:40:02 -0300

    Serialização

--- src/academy/devdojo/maratonajava/javacore/Xserializacao/test/SerializacaoTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Xserializacao/test/SerializacaoTest01.java
@@ -1,4 +1,39 @@
 package academy.devdojo.maratonajava.javacore.Xserializacao.test;
 
+import academy.devdojo.maratonajava.javacore.Xserializacao.dominio.Aluno;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class SerializacaoTest01 {
+    static void main(String[] args) {
+        Aluno aluno = new Aluno(1L, "Lucca", "123456");
+        serializar(aluno);
+        deserializar();
+    }
+
+    private static void serializar (Aluno aluno) {
+        Path path = Paths.get("aluno.ser");
+        try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(path))) {
+          oos.writeObject(aluno);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void deserializar () {
+        Path path = Paths.get("aluno.ser");
+        try (ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(path))) {
+            Aluno aluno1 = (Aluno) ois.readObject();
+            System.out.println(aluno1);
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
