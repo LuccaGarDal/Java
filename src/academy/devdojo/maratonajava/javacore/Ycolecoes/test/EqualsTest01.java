3540499dedbd50d1b074cb0eeffefd5490fc67fb
LuccaGarDal <luccagarciarosa@gmail.com>
2025-09-25 19:24:57 -0300

    hashCode e sobrescrita

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/EqualsTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/EqualsTest01.java
@@ -7,6 +7,13 @@ public class EqualsTest01 {
         Smartphone s1 = new Smartphone("12345", "iPhone");
         Smartphone s2 = new Smartphone("12345", "iPhone");
         System.out.println(s1.equals(s2));
+        int hashS1 = s1.hashCode();
+        int hashS2 = s2.hashCode();
+        if (hashS1 == hashS2) {
+            System.out.println(hashS1 + " e " + hashS2);
+        } else {
+            System.out.println();
+        }
 
     }
 
