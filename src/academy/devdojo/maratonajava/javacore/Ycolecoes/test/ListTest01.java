d1bd21de55797a11f91fadd4b55e0af96ba29903
LuccaGarDal <luccagarciarosa@gmail.com>
2025-09-25 21:09:08 -0300

    List (add, remove, addAll)

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
@@ -5,10 +5,19 @@ import java.util.List;
 
 public class ListTest01 {
     static void main(String[] args) {
-        List <String> nomes = new ArrayList<>();
+        List <String> nomes = new ArrayList<>(16);
+        List <String> nomes2 = new ArrayList<>(16);
         nomes.add("Lucca");
         nomes.add("Jous");
         nomes.add("Julia");
+
+        nomes2.add("Garcia");
+        nomes2.add("Ions");
+
+        nomes.remove(0);
+        nomes.remove("Jous");
+
+        nomes.addAll(nomes2);
 
         for (String nome : nomes) {
             System.out.println(nome);
