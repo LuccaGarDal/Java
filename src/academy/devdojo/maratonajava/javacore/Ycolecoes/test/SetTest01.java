89ef68e1329abf7137aa54fe95bc039f301cbf11
LuccaGarDal <luccagarciarosa@gmail.com>
2025-09-30 16:16:06 -0300

    Set e HashSet

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/SetTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/SetTest01.java
@@ -2,33 +2,21 @@ package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
 
 import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Manga;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
-public class IteratorTest01 {
+public class SetTest01 {
     static void main(String[] args) {
-        List<Manga> mangas = new LinkedList<>();
+        Set<Manga> mangas = new HashSet<>();
         mangas.add(new Manga(5L, "One Piece", 25));
         mangas.add(new Manga(2L, "Dragon Ball", 25));
         mangas.add(new Manga(3L, "Naruto", 25, 1));
         mangas.add(new Manga(1L, "Bleach", 25));
         mangas.add(new Manga(4L, "Berserk", 25));
 
-        Iterator<Manga> mangaIterator = mangas.iterator();
-        while (mangaIterator.hasNext()) {
-            if (mangaIterator.next().getQuantidade() == 0) {
-                mangaIterator.remove();
-            }
-        }
         for (Manga manga : mangas) {
             System.out.println(manga);
-
         }
-
-//        mangas.removeIf(manga -> manga.getQuantidade() == 0);
-//        System.out.println(mangas);
 
     }
 }
