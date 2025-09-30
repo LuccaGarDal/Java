cecbf53ffc09d01ba9a236bb03894db3d8093769
LuccaGarDal <luccagarciarosa@gmail.com>
2025-09-30 18:52:35 -0300

    Exercicio 1 fazendo list e removendo numeros pares

---
+++ src/academy/devdojo/maratonajava/exercicios/ExerciciosList/ExercicioList01.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.exercicios.ExerciciosList;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ExercicioList01 {
+    static void main() {
+        List<Integer> numeros = new ArrayList<>();
+
+        for (int i = 1; i < 21; i++) {
+            numeros.add(i);
+
+        }
+        System.out.println(numeros);
+
+        numeros.removeIf(numero -> numero % 2 == 0);
+
+        System.out.println(numeros);
+
+    }
+}
