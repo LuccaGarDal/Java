e073bf385a2d37e292e8be68af2dfff29b1337b6
LuccaGarDal <luccagarciarosa@gmail.com>
2025-10-03 15:57:28 -0300

    Wildcard Introducao

---
+++ src/academy/devdojo/maratonajava/javacore/Zgenerics/WildcardTest01.java
@@ -0,0 +1,41 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics;
+
+abstract class Animal {
+    public abstract void consulta();
+}
+
+class Cachorro extends Animal {
+
+    @Override
+    public void consulta() {
+        System.out.println("Consultando Cachorro");
+    }
+}
+
+class Gato extends Animal {
+
+    @Override
+    public void consulta() {
+        System.out.println("Consultando Gato");
+    }
+}
+
+public class WildcardTest01 {
+    static void main() {
+        Gato [] gatos = {new Gato(), new Gato()};
+        Cachorro[] cachorros = {new Cachorro(), new Cachorro()};
+        printConsulta(gatos);
+        printConsulta(cachorros);
+        Animal[] animals = {new Gato(), new Cachorro()};
+        Animal animal  = new Gato();
+        printConsulta(animals);
+    }
+
+    private static void printConsulta(Animal[] animals) {
+        for (Animal animal : animals) {
+            animal.consulta();
+        }
+
+    }
+
+}
