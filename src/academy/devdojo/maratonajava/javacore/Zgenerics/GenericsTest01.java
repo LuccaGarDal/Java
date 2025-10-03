2def977a77b6bad5b1c3c4874883fadce8bc8397
LuccaGarDal <luccagarciarosa@gmail.com>
2025-10-03 15:38:57 -0300

    Generics Introducao

---
+++ src/academy/devdojo/maratonajava/javacore/Zgenerics/GenericsTest01.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics;
+
+import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Consumidor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericsTest01 {
+    static void main(String[] args) {
+        List<String> lista = new ArrayList<>();
+        lista.add("Lucca");
+        lista.add("Lucca");
+
+        for (String o : lista) {
+            System.out.println(o);
+        }
+
+        add(lista, new  Consumidor("Lucca"));
+
+        for (Object o : lista) {
+            System.out.println(o);
+        }
+
+    }
+
+    private static void add (List lista, Consumidor consumidor) {
+        lista.add(consumidor);
+    }
+}
