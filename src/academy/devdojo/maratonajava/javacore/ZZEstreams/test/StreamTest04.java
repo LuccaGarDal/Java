9d29eade0cc0d664becb37094def355ae0851ca0
LuccaGarDal <luccagarciarosa@gmail.com>
2025-10-14 16:01:23 -0300

    Streams 4 (FlatMap)

---
+++ src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest04.java
@@ -0,0 +1,31 @@
+package academy.devdojo.maratonajava.javacore.ZZEstreams.test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamTest04 {
+    static void main(String[] args) {
+        List<List<String>> kingdom = new ArrayList<>();
+        List<String> cavaleiros = List.of("Lucca", "Ions", "Jous");
+        List<String> reis = List.of("Garcia");
+        List<String> magos = List.of("Lin", "Pedro");
+        kingdom.add(cavaleiros);
+        kingdom.add(reis);
+        kingdom.add(magos);
+
+        for (List<String> list : kingdom) {
+            for (String s : list) {
+                System.out.println(s);
+            }
+
+        }
+        System.out.println("---------------");
+        kingdom.stream()
+                .flatMap(Collection::stream)
+                .forEach(System.out::println);
+
+
+    }
+}
