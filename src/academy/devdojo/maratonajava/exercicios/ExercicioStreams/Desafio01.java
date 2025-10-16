8937c8eae98eb8578d731c4ba645c8722a15c891
LuccaGarDal <luccagarciarosa@gmail.com>
2025-10-16 20:08:18 -0300

    Exercicio Streams 13 (Desafio1)

--- src/academy/devdojo/maratonajava/exercicios/ExercicioStreams/Desafio01.java
+++ src/academy/devdojo/maratonajava/exercicios/ExercicioStreams/Desafio01.java
@@ -1,4 +1,51 @@
 package academy.devdojo.maratonajava.exercicios.ExercicioStreams;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 public class Desafio01 {
+    public static List<String> frase = List.of(
+            "o java é divertido",
+            "streams são poderosas",
+            "lambda simplifica o código"
+    );
+
+    static void main(String[] args) {
+
+        long count = frase.stream()
+                .map(i -> i.split(" "))
+                .flatMap(Arrays::stream)
+                .count();
+
+        System.out.println(count);
+
+        long count1 = frase.stream()
+                .map(i -> i.split(" "))
+                .flatMap(Arrays::stream)
+                .filter(i -> i.length() > 5)
+                .count();
+
+        System.out.println(count1);
+
+        frase.stream()
+                .map(i -> i.split(" "))
+                .flatMap(Arrays::stream)
+                .reduce((s, s2) -> s.length() > s2.length() ? s : s2)
+                .ifPresent(System.out::println);
+
+        List<String> list = frase.stream()
+                .map(i -> i.split(" "))
+                .flatMap(Arrays::stream)
+                .toList();
+
+        System.out.println(list);
+
+        boolean temJava = frase.stream()
+                .map(i -> i.split(" "))
+                .flatMap(Arrays::stream)
+                .anyMatch(s -> s.contains("java"));
+
+        System.out.println(temJava);
+    }
 }
--- src/academy/devdojo/maratonajava/exercicios/ExercicioStreams/TudoJunto01.java
+++ src/academy/devdojo/maratonajava/exercicios/ExercicioStreams/TudoJunto01.java
@@ -1,4 +1,43 @@
 package academy.devdojo.maratonajava.exercicios.ExercicioStreams;
 
+import java.util.Collection;
+import java.util.List;
+
 public class TudoJunto01 {
+    public static List<List<Integer>> matriz = List.of(
+            List.of(1, 2, 3),
+            List.of(4, 5, 6),
+            List.of(7, 8, 9)
+    );
+
+    static void main(String[] args) {
+
+        matriz.stream()
+                .flatMap(Collection::stream)
+                .forEach(System.out::println);
+
+        matriz.stream()
+                .flatMap(Collection::stream)
+                .reduce(Integer::sum)
+                .ifPresent(System.out::println);
+
+        boolean todosSaoMenorQue10 = matriz.stream()
+                .flatMap(Collection::stream)
+                .allMatch(s -> s < 10);
+
+        System.out.println(todosSaoMenorQue10);
+
+        matriz.stream()
+                .flatMap(Collection::stream)
+                .filter(s -> s > 4)
+                .findFirst()
+                .ifPresent(System.out::println);
+
+        matriz.stream()
+                .flatMap(Collection::stream)
+                .filter(s -> s % 2 != 0)
+                .reduce((i1, i2) -> i1 * i2)
+                .ifPresent(System.out::println);
+
+    }
 }
