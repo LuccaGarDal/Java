bba511b3c57938e0ed04d410efd2d8e1de392767
LuccaGarDal <luccagarciarosa@gmail.com>
2025-10-20 20:21:50 -0300

    Exercicio Streams 20 (FindAny e FindFirst)

--- src/academy/devdojo/maratonajava/exercicios/ExercicioStreams/FindFirstAny.java
+++ src/academy/devdojo/maratonajava/exercicios/ExercicioStreams/FindFirstAny.java
@@ -4,11 +4,17 @@ import java.util.List;
 
 public class FindFirstAny {
     static void main(String[] args) {
-        List<String> palavras = List.of("stream", "lambada", "flatmap", "java", "compilation");
+        List<String> palavras = List.of("stream", "lambda", "flatmap", "java", "compilation");
 
         palavras.stream()
                 .filter(s -> s.length() > 5)
                 .findFirst()
                 .ifPresent(System.out::println);
+
+        palavras.stream()
+                .filter(s -> s.contains("a"))
+                .findAny()
+                .ifPresent(System.out::println);
+
     }
 }
