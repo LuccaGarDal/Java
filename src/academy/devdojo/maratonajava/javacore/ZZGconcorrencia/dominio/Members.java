cce39d27e22407586a0d5ede872ffe16173bfaa5
LuccaGarDal <luccagarciarosa@gmail.com>
2025-10-24 15:55:30 -0300

    Concorrência 3 (Lock e Condition)

--- src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/dominio/Members.java
+++ src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/dominio/Members.java
@@ -1,10 +1,15 @@
-package academy.devdojo.maratonajava.javacore.ZZFthreads.dominio;
+package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.dominio;
 
 import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Members {
     private final Queue<String> emails = new ArrayBlockingQueue<>(10);
+    private final ReentrantLock lock = new ReentrantLock();
+    private final Condition condition = lock.newCondition();
     private boolean open = true;
 
     public boolean isOpen() {
@@ -12,37 +17,49 @@ public class Members {
     }
 
     public int pendingEmails() {
-        synchronized (emails) {
+        lock.lock();
+        try {
             return emails.size();
+        } finally {
+            lock.unlock();
         }
     }
 
-    public void addMemberEmail (String email) {
-        synchronized (this.emails) {
+    public void addMemberEmail(String email) {
+        lock.lock();
+        try {
             String threadname = Thread.currentThread().getName();
             System.out.println(threadname + " adicionou email na lista.");
             this.emails.add(email);
-            this.emails.notifyAll();
+            condition.signalAll();
+        } finally {
+            lock.unlock();
         }
     }
 
-    public String retrieveEmail () throws InterruptedException {
+    public String retrieveEmail() throws InterruptedException {
         System.out.println(Thread.currentThread().getName() + " checking if there are emails");
-        synchronized (this.emails) {
-            while(this.emails.isEmpty()) {
+        lock.lock();
+        try {
+            while (this.emails.isEmpty()) {
                 if (!open) return null;
                 System.out.println(Thread.currentThread().getName() + " não há email disponível na lista, entrando em modo de espera");
-                this.emails.wait();
+                condition.await();
             }
             return this.emails.poll();
+        } finally {
+            lock.unlock();
         }
     }
 
-    public void close () {
+    public void close() {
         open = false;
-        synchronized (this.emails) {
+        lock.lock();
+        try {
             System.out.println(Thread.currentThread().getName() + " notificando a todos que não estamos enviando mais emails.");
-            this.emails.notifyAll();
+            condition.signalAll();
+        } finally {
+            lock.unlock();
         }
     }
 }
